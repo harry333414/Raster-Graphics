9e5392c0cac0065dd6dd505de1132351eebfdcf4
harry333414 <hristo.chocho@gmail.com>
2023-05-18 14:14:49 +0300

    Started working on a basic Converter File

---
+++ Files/ConvertedFile.cpp
@@ -0,0 +1,2 @@
+#include "ConvertedFile.hpp"
+
---
+++ Files/ConvertedFile.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::size_t;
+using std::vector;
+
+class ConvertedFile {
+
+    private:
+
+        size_t type;
+        string description;
+        size_t max_value;
+        vector<vector<size_t>> grid;
+
+    public:
+
+        
+
+};
